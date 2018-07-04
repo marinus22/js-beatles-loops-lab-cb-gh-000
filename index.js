275cb3f861fb905f9b75fd93c63b4d154bda4350
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-04 12:06:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,12 @@ function johnLennonFacts(facts) {
   }
   return facts;
 }
+
+function iLoveTheBeatles(n) {
+  var empty = [];
+  do {
+    empty.push('I love the Beatles!');
+    n++;
+  } while (n<15);
+  return empty;
+}
