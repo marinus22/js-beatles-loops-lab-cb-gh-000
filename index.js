6cfcdb3023e4c6b6ec343782d081d02108a1c180
Sara McCarthy <sc.mccarthy1123@gmail.com>
2018-07-04 11:58:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var plays = [];
+
+  for (var i =0; i < musicians.length;
+  i = i + 1) {
+    plays[i] = musicians[i] + ' plays ' + instruments[i];
+  }
+
+  return plays;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + '!!!';
+    i = i +1;
+  }
+  return facts;
+}
